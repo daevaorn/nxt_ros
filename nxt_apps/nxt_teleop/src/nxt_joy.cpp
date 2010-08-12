4e7ebaccc64854b7a2460c3bca35a37220fd69d1
Tully Foote <tully.foote@gmail.com>
2010-08-12 11:41:52 -0700

    nxt_joy now with deadman support

--- nxt_apps/nxt_teleop/src/nxt_joy.cpp
+++ nxt_apps/nxt_teleop/src/nxt_joy.cpp
@@ -1,27 +1,36 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <joy/Joy.h>
+#include "boost/thread/mutex.hpp"
+#include "boost/thread/thread.hpp"
 
-class NXTTeleop
+class NxtTeleop
 {
 public:
-  NXTTeleop();
+  NxtTeleop();
 
 private:
   void joyCallback(const joy::Joy::ConstPtr& joy);
+  void publish();
 
   ros::NodeHandle nh_;
 
-  int linear_, angular_;
+  int linear_, angular_, deadman_axis_;
   double l_scale_, a_scale_;
   ros::Publisher vel_pub_;
   ros::Subscriber joy_sub_;
 
+  geometry_msgs::Twist last_published_;
+  boost::mutex publish_mutex_;
+  bool deadman_pressed_;
+  ros::Timer timer_;
+  
 };
 
-NXTTeleop::NXTTeleop():
+NxtTeleop::NxtTeleop():
   linear_(1),
   angular_(2),
+  deadman_axis_(0),
   l_scale_(50.0),
   a_scale_(2.0)
 {
@@ -33,21 +42,37 @@ NXTTeleop::NXTTeleop():
 
 
   vel_pub_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel", 1);
-  joy_sub_ = nh_.subscribe<joy::Joy>("joy", 10, &NXTTeleop::joyCallback, this);
+  joy_sub_ = nh_.subscribe<joy::Joy>("joy", 10, &NxtTeleop::joyCallback, this);
+
+  timer_ = nh_.createTimer(ros::Duration(0.1), boost::bind(&NxtTeleop::publish, this));
 }
 
-void NXTTeleop::joyCallback(const joy::Joy::ConstPtr& joy)
+void NxtTeleop::joyCallback(const joy::Joy::ConstPtr& joy)
 {
   geometry_msgs::Twist vel;
   vel.angular.z = a_scale_*joy->axes[angular_];
   vel.linear.x = l_scale_*joy->axes[linear_];
-  vel_pub_.publish(vel);
+  last_published_ = vel;
+  deadman_pressed_ = joy->axes[deadman_axis_];
+
 }
 
+void NxtTeleop::publish()
+{
+  boost::mutex::scoped_lock lock(publish_mutex_);  
+  if (deadman_pressed_)
+    {
+      vel_pub_.publish(last_published_);
+    }
+  
+}
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "nxt_teleop");
-  NXTTeleop nxt_teleop;
+  NxtTeleop nxt_teleop;
+
+  ros::NodeHandle n;
+  
 
   ros::spin();
 }
